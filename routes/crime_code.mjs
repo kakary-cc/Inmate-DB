7c6905487c2bdefafef43f12a1016a15ce960445
TR-9NF462 <mingxuanz@mail.kakari.cc>
2024-04-20 20:49:06 -0400

    Add crime code deletion feature.

--- routes/crime_code.mjs
+++ routes/crime_code.mjs
@@ -10,15 +10,19 @@ class Crime_code {
         this.description = description.trim();
     }
     list = () => `SELECT * FROM Crime_codes;`;
-    search = () =>
-        `SELECT * FROM Crime_codes WHERE Crime_code LIKE "${this.code}";`;
     insert = () =>
         `INSERT INTO Crime_codes (Crime_code, Code_description) VALUES ("${this.code}", "${this.description}");`;
+    delete = () => `DELETE FROM Crime_codes WHERE Crime_code = ${this.code};`;
 }
 
 app.get("/", async (req, res) => {
-    const result = (await db.query(new Crime_code().list()))[0];
-    res.render("./crime_code", { Crime_code: result });
+    try {
+        const result = (await db.query(new Crime_code().list()))[0];
+        res.render("./crime_code", { Crime_code: result });
+    } catch (error) {
+        console.log(error);
+        res.status(500).render("error", { error: "Internal Error." });
+    }
 });
 
 app.get("/new", async (req, res) => {
@@ -31,15 +35,27 @@ app.post("/new", async (req, res) => {
         res.redirect("./");
     } catch (error) {
         console.log(error);
-        res.send(
-            error.errno === 1062
-                ? `<script>
-                alert("Duplicate key!");
-                window.location.href = "/crime_code/new";</script>`
-                : `<script>
-                alert("Error occurred when adding new entry.");
-                window.location.href = "/crime_code/new";</script>`
-        );
+        if (error.errno === 1062) {
+            res.send(`<script>alert("Duplicate key!");
+            window.location.href = "/crime_code/new";</script>`);
+        } else {
+            res.status(500).render("error", { error: "Internal Error." });
+        }
+    }
+});
+
+// DELETE
+app.post("/:code", async (req, res) => {
+    if (req.query.method?.toUpperCase() !== "DELETE") {
+        res.status(400).render("error", { error: "Bad Request." });
+        return;
+    }
+    try {
+        await db.query(new Crime_code(req.params).delete());
+        res.redirect("./");
+    } catch (error) {
+        console.log(error);
+        res.status(500).render("error", { error: "Internal Error." });
     }
 });
 
