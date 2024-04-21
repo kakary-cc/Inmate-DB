31fe7c86dad4f1814587d089d91dd9e75e73eb38
TR-9NF462 <mingxuanz@mail.kakari.cc>
2024-04-20 20:19:09 -0400

    Add new crime code page

--- routes/crime_code.mjs
+++ routes/crime_code.mjs
@@ -4,13 +4,15 @@ import db from "../db.mjs";
 const app = express();
 
 // Schema
-function Crime_code(code, description) {
-    this.code = code;
-    this.description = description;
-    this.list = () => `SELECT * FROM Crime_codes;`;
-    this.search = () =>
+class Crime_code {
+    constructor({ code = "", description = "" } = {}) {
+        this.code = parseInt(code);
+        this.description = description.trim();
+    }
+    list = () => `SELECT * FROM Crime_codes;`;
+    search = () =>
         `SELECT * FROM Crime_codes WHERE Crime_code LIKE "${this.code}";`;
-    this.insert = () =>
+    insert = () =>
         `INSERT INTO Crime_codes (Crime_code, Code_description) VALUES ("${this.code}", "${this.description}");`;
 }
 
@@ -21,6 +23,24 @@ app.get("/", async (req, res) => {
 
 app.get("/new", async (req, res) => {
     res.render("./crime_code/new", {});
+});
+
+app.post("/new", async (req, res) => {
+    try {
+        await db.query(new Crime_code(req.body).insert());
+        res.redirect("./");
+    } catch (error) {
+        console.log(error);
+        res.send(
+            error.errno === 1062
+                ? `<script>
+                alert("Duplicate key!");
+                window.location.href = "/crime_code/new";</script>`
+                : `<script>
+                alert("Error occurred when adding new entry.");
+                window.location.href = "/crime_code/new";</script>`
+        );
+    }
 });
 
 export { app as crimeCodeRoutes };
