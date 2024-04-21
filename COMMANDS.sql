856ae5e49dc7e09fce4640e10c89e8c5358ef5c6
TR-9NF462 <mingxuanz@mail.kakari.cc>
2024-04-21 17:47:06 -0400

    cleanup

--- COMMANDS.sql
+++ COMMANDS.sql
@@ -346,6 +346,34 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
+CREATE PROCEDURE insert_officer(
+    IN p_Last VARCHAR(15),
+    IN p_First VARCHAR(10),
+    IN p_Precinct CHAR(4),
+    IN p_Badge VARCHAR(14),
+    IN p_Phone CHAR(10),
+    IN p_Status CHAR(1)
+)
+BEGIN
+    INSERT INTO Officers (
+        Last,
+        First,
+        Precinct,
+        Badge,
+        Phone,
+        Status
+    ) VALUES (
+        p_Last,
+        p_First,
+        p_Precinct,
+        p_Badge,
+        p_Phone,
+        p_Status
+    );
+END$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE PROCEDURE insert_prob_officer(
     IN p_Last VARCHAR(15),
     IN p_First VARCHAR(10),
--- README.md
+++
@@ -1,8 +0,0 @@
-#### Useful Information:
-
----
-
-> [MySQL Injection](https://planetscale.com/blog/how-to-prevent-sql-injection-attacks-in-node-js)
-
-
-> [MySQL JS Documentation](https://sidorares.github.io/node-mysql2/docs)
